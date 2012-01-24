cc08a6e330994c949f73f8d5b940886ddedc5a6e
Daniel Johnson <danjohns@mymail.mines.edu>
2012-01-23 21:23:28 -0700

    completed rubywarrior level 3

--- project02/rubywarrior/baldr-beginner/player.rb
+++ project02/rubywarrior/baldr-beginner/player.rb
@@ -1,9 +1,11 @@
 class Player
   def play_turn(warrior)
-		if warrior.feel.empty?
-			warrior.walk!
-		else
+		if !warrior.feel.empty?
 			warrior.attack!
+		elsif warrior.health < 20
+			warrior.rest!
+		else
+			warrior.walk!
 		end
   end
 end
