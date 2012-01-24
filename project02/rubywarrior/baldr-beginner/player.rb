5cb3e564f3d3f35a06aaa09786b2672111b0361f
Daniel Johnson <danjohns@mymail.mines.edu>
2012-01-23 21:47:18 -0700

    Completed level 6

--- project02/rubywarrior/baldr-beginner/player.rb
+++ project02/rubywarrior/baldr-beginner/player.rb
@@ -1,15 +1,28 @@
 class Player
   def play_turn(warrior)
+
 		if !warrior.feel.empty?
 			unless warrior.feel.captive?
 				warrior.attack!
 			else
 				warrior.rescue!
 			end
-		elsif warrior.health < 20 and warrior.health >= @health
-			warrior.rest!
+		elsif warrior.health < 20 and @health <= warrior.health
+			if warrior.feel(:backward).empty?
+				warrior.walk!(:backward)
+			else
+				if warrior.feel(:backward).wall?
+					warrior.rest!
+				else
+					warrior.rescue!(:backward)
+				end
+			end
 		else
-			warrior.walk!
+			if warrior.health < 10
+				warrior.walk!(:backward)
+			else
+				warrior.walk!
+			end
 		end
 		@health = warrior.health
   end
