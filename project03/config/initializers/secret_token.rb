6b676a6aa69150ed0de3b94d366e79c4c4e4a524
Daniel Johnson <danjohns@mymail.mines.edu>
2012-02-05 13:40:34 -0700

    Adding init files

---
+++ project03/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project03::Application.config.secret_token = '880a120d7ae102293885d715557dbb4450999e78a55c7f1d38016ef91d1e904753ad2aa990822fe96e91a0a0823bd51841b8339e6fa69b37728e4e9ec955cdce'
