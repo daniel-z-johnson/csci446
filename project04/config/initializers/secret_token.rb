574debe8ce48052190127dd537b545ca78eb6489
Daniel Johnson <danjohns@mymail.mines.edu>
2012-02-18 19:43:39 -0700

    Init project04 files plus scaffold for article

---
+++ project04/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = '5fec50e61d27b1ff2e7fae1b7540287094033c4f6331e91fdaa9e4f24670ccc2c7eccd4e08fc2a3df2e68618d517b1c6c8f5bed5ac46067647d4c941195bb3f6'
