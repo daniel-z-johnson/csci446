974de9ef9233f83f0f4ddcf8664253dd38456f9f
Daniel Johnson <danjohns@mymail.mines.edu>
2012-02-20 20:27:35 -0700

    Added subtutles to rest of show pages and added time formats to enviroment and show pages

--- project04/config/environment.rb
+++ project04/config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Articles::Application.initialize!
+Time::DATE_FORMATS[:show]="%b. %d, %Y."
+Time::DATE_FORMATS[:index]="%A, %b. %d %Y."
