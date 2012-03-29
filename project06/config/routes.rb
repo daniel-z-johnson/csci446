7f8d848bb8f5b7bc7b879472490657ff8f302955
Daniel Johnson <danjohns@mymail.mines.edu>
2012-03-28 23:03:59 -0600

    Change routes, had some problems, may still have problems with logout

--- project06/config/routes.rb
+++ project06/config/routes.rb
@@ -1,5 +1,5 @@
 Gamez::Application.routes.draw do
-  devise_for :users, :path_names => {:sign_up => "new"}
+  devise_for :users, :path_names => { :sign_up => 'new', :sign_out => 'logout', :sign_in => 'login'}
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,7 +51,7 @@ Gamez::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  #root :to => 'users#new'
+  #root :to => ''
 
   # See how all your routes lay out with "rake routes"
 
