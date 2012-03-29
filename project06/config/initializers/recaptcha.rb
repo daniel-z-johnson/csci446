b5634a0c52c94046e1e9b3537346e580d1135efe
Daniel Johnson <danjohns@mymail.mines.edu>
2012-03-28 21:07:12 -0600

    Added recaptcha gem and in process of adding it to registration page

---
+++ project06/config/initializers/recaptcha.rb
@@ -0,0 +1,5 @@
+Recaptcha.configure do |config|
+	config.public_key  = '6Ldeis8SAAAAAJ8nKo7yAN4BJDIUGsUUvoDZTMty'
+	config.private_key = '6Ldeis8SAAAAAHmjalT_6O3lidHfacFAO1SWaZGD'
+	##config.proxy = 'http://myrpoxy.com.au:8080'
+end
