55dcb3711f1d3507265d866959034101dacd00e0
Daniel Johnson <danjohns@mymail.mines.edu>
2012-03-28 20:10:21 -0600

    Added first and last name to users table

--- project06/db/schema.rb
+++ project06/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120329014309) do
+ActiveRecord::Schema.define(:version => 20120329015839) do
 
   create_table "users", :force => true do |t|
     t.string   "email",               :default => "",    :null => false
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(:version => 20120329014309) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.boolean  "admin",               :default => false
+    t.string   "first_name",          :default => "",    :null => false
+    t.string   "last_name",           :default => "",    :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
