4aa3712bb18f1659f57fae66a0da1918ee6607b0
Daniel Johnson <danjohns@mymail.mines.edu>
2012-03-28 22:09:22 -0600

    fixed bug, forgot to do a migration to add username, facepalm

--- project06/db/schema.rb
+++ project06/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120329015839) do
+ActiveRecord::Schema.define(:version => 20120329035940) do
 
   create_table "users", :force => true do |t|
     t.string   "email",               :default => "",    :null => false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20120329015839) do
     t.boolean  "admin",               :default => false
     t.string   "first_name",          :default => "",    :null => false
     t.string   "last_name",           :default => "",    :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
