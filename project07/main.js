d592463eac3d04d13b3b35ab488922c1ba1af992
Daniel Johnson <danjohns@mymail.mines.edu>
2012-04-17 15:50:46 -0600

    Able to get user input now, no real game logic yet, it is coming though

--- project07/main.js
+++ project07/main.js
@@ -1,6 +1,7 @@
 var guessesLeft = 10;
 var highScores = new Array([9, "HarryJamesPotter"], [3, "ZedCthulhu"], [2, "NearlyDied"]);
 var number=0;
+var theGuess;
 
 function generateRandomNumber(){
 	number = Math.floor((Math.random()*100)+1);
@@ -23,5 +24,13 @@ function populateHighScores(scores) {
 }
 
 function updateScore(score) {
+  $('h2#score span#guessesLeft').empty();
   $('h2#score span#guessesLeft').append(score);
+}
+
+function guessNumber(){
+	theGuess = $('input#guess').val();
+	alert(theGuess);
+	--guessesLeft;
+	updateScore(guessesLeft);
 }
